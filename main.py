ad760552157782084042db8a738bdf591d17e1a1
Susana Chang <samanta.chang@utec.edu.pe>
2022-03-24 16:20:07 -0500

    problema 8 ejemplo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from math import pi, cos, sqrt
 
 ruta1 = "Relación de instituciones y programas educativos.csv"
 ruta2 = "Títulos registrados de Institutos de Educación Superior Tecnológicas^J Instituciones de Educación Superior 2019-2020.csv"
@@ -9,6 +10,7 @@ departamentos_dict = {}
 lista_paginas_web = []
 fechas_dict = {}
 lista_centros = []
+matriz_datos = []
 
 with open(ruta1, newline='', encoding="latin-1") as csv1:
     # se salta el header
@@ -39,7 +41,27 @@ with open(ruta1, newline='', encoding="latin-1") as csv1:
             else:
                 fechas_dict[fecha[2]] += 1
 
+        # pregunta 8
+        matriz_datos.append(row)
 
+def distancia(lat1, lat2, long1, long2):
+
+    delta_lat = lat1 * pi / 180 - lat2 * pi / 180
+    delta_long = long1 * pi / 180 - long2 * pi / 180
+    prom_lat = (lat1 * pi / 180 + lat2 * pi / 180)/2.0
+    d = 6371.009 * sqrt(delta_lat ** 2 + (delta_long * cos(prom_lat)) ** 2)
+
+    return d
+
+mayor_dist = 0
+centro1 = matriz_datos[0][3]
+centro2 = ""
+
+for i in matriz_datos:
+    d = distancia(float(matriz_datos[0][32]), float(i[32]), float(matriz_datos[0][33]), float(i[33]))
+    if d > mayor_dist:
+        mayor_dist = d
+        centro2 = i[3]
 
 # 1. Indique los 3 departamentos que cuentan con más locales educativos.
 cantidad_departamento = sorted(departamentos_dict.items(), key=lambda x: x[1], reverse=True)[0:3]
@@ -69,3 +91,5 @@ print(f"El anho con mas centros educativos fue {anho_mas_centros[0]} con {anho_m
 
 print("\n-----\n")
 
+# 8. ¿Es posible saber qué centros educativos están muy alejados uno del otro?
+print(f"Ejemplo:\nLa mayor distancia entre el centro educativo {centro1} y el {centro2} es {mayor_dist}.")
