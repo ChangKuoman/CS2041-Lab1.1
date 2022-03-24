079f9a28b4c4393e12ff7695fb860adbb48f3fe8
Susana Chang <samanta.chang@utec.edu.pe>
2022-03-24 15:28:21 -0500

    agrega problema 3 y 4

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ ruta2 = "Títulos registrados de Institutos de Educación Superior Tecnológicas
 
 departamentos_dict = {}
 lista_paginas_web = []
+fechas_dict = {}
+lista_centros = []
 
 with open(ruta1, newline='', encoding="latin-1") as csv1:
     # se salta el header
@@ -25,6 +27,19 @@ with open(ruta1, newline='', encoding="latin-1") as csv1:
         if ".edu.pe" in row[16]:
             lista_paginas_web.append([row[3], row[16]])
 
+        # pregunta 3
+        if (row[8] == "Mujeres") and (row[37] == "Noche") and (row[39] == "Activa"):
+            lista_centros.append(row[3])
+
+        # pregunta 4
+        fecha = row[40].split("-")
+        if len(fecha) == 3:
+            if fecha[2] not in fechas_dict.keys():
+                fechas_dict[fecha[2]] = 1
+            else:
+                fechas_dict[fecha[2]] += 1
+
+
 
 # 1. Indique los 3 departamentos que cuentan con más locales educativos.
 cantidad_departamento = sorted(departamentos_dict.items(), key=lambda x: x[1], reverse=True)[0:3]
@@ -32,8 +47,25 @@ print("Departamentos con más locales educativos:")
 for i in range(3):
     print(f"({i+1}) Departamento: {cantidad_departamento[i][0]}. Cantidad: {cantidad_departamento[i][1]}")
 
+print("\n-----\n")
 
 # 2. Liste aquellos centros educativos que cuenten con una página web y su dominio termine en ’.edu.pe’.
 print("Lista de centros educativos que cuentas con pagina web y dominio .edu.pe:")
 for i in lista_paginas_web:
     print(f"Centro educativo: {i[0]}. Pagina web: {i[1]}")
+
+print("\n-----\n")
+
+# 3 Identifique aquellos centros educativos que permitan solo mujeres, donde el turno sea de noche y no esten inhabilitados.
+print("Centros educativos que permitan solo mujeres, donde el turno sea de noche y no esten inhabilitados.")
+for i in lista_centros:
+    print(f"Centro educativo: {i}")
+
+print("\n-----\n")
+
+# 4. ¿En qué año se crearon más centros educativos?
+anho_mas_centros = sorted(fechas_dict.items(), key=lambda x: x[1], reverse=True)[0]
+print(f"El anho con mas centros educativos fue {anho_mas_centros[0]} con {anho_mas_centros[1]} creados.")
+
+print("\n-----\n")
+
